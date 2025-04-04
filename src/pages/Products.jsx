2ab4428128ce02645448c412eed43efdace46d0f
Alex de 0. Vidal <contato@webav.com.br>
2025-04-04 00:17:15 -0300

    Add src/pages/Products.jsx

---
+++ src/pages/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Products() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Produtos</h1>
+      <p>Confira nossa variedade de produtos disponíveis.</p>
+      {/* Aqui você pode adicionar uma lista de produtos */}
+    </div>
+  );
+}
+
+export default Products;
