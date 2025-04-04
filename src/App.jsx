1eba3d631a96caf788fd7b07b943364c7b628b28
Alex de 0. Vidal <contato@webav.com.br>
2025-04-04 00:17:11 -0300

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/products" component={Products} />
+        <Route path="/cart" component={Cart} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
