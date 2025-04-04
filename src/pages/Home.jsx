f4d4d6ec2641a7be9c73063ad4a54d47b9e62897
Alex de 0. Vidal <contato@webav.com.br>
2025-04-04 00:17:14 -0300

    Add src/pages/Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Bem-vindo à nossa Loja de Roupas!</h1>
+      <p>Explore nossa coleção exclusiva de roupas e encontre o que combina com você.</p>
+    </div>
+  );
+}
+
+export default Home;
