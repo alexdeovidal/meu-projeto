957a05e30d9157e672759b37948bad44ca172c51
Alex de 0. Vidal <contato@webav.com.br>
2025-04-04 00:17:13 -0300

    Add src/pages/Cart.jsx

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Carrinho de Compras</h1>
+      <p>Veja os itens que você adicionou ao seu carrinho.</p>
+      {/* Aqui você pode adicionar a lista de itens no carrinho */}
+    </div>
+  );
+}
+
+export default Cart;
